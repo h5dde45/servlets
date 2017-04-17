1d3638212f372aeba8e472fa24d90b86d9486cdf
nov <nov@mail.ru>
2017-04-17 23:40:34 +0400

    Commit 1

---
+++ src/servlet/TestServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/TestServlet")
+public class TestServlet extends HttpServlet {
+
+    protected void progressRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=response.getWriter();
+        try{
+            out.println("<html>");
+            out.println("<body>");
+
+            out.println("111======");
+            out.println(request.getContextPath());
+            out.println("</body>");
+            out.println("</html>");
+        }finally {
+            out.close();
+        }
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        progressRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        progressRequest(request, response);
+    }
+
+}
